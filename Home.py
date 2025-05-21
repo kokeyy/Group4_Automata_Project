610e8e6c7d2cc8995bc46df09e52baba889a31b2
Charles Lawrence P. Austria <144287942+kokeyy@users.noreply.github.com>
2025-05-21 21:49:37 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,23 @@
 import streamlit as st
+import pathlib
 
-st.title("Welcome to the Automaton App")
+# Function to load CSS from the 'assets' folder
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-if st.button("First DFA"):
-    st.switch_page("Pages/DFA1.py")
-if st.button("Second DFA"):
-    st.switch_page("Pages/DFA2.py")
+# Load the external CSS
+css_path = pathlib.Path("assets/styles.css")
+load_css(css_path)
+
+st.html("<h1 style = 'text-align: center;'> Group Project in Automata</h1>")
+st.html("<h2 style = 'text-align: center;'> By Group 4</h2>")
+
+col1, col2, col3, col4, col5 = st.columns([1, 2, 1, 2, 1])
+
+with col2:
+    if st.button("1st DFA", key = 'pulse'):
+        st.switch_page("Pages/DFA1.py")
+with col4:        
+    if st.button("2nd DFA", key = 'pulse2'):
+        st.switch_page("Pages/DFA2.py")
